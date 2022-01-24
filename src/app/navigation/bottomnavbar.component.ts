63af7dea7c092a95826b2fb5c0e50e88f7cdc806
Jared <jaw9801@gmail.com>
2022-01-24 18:41:00 -0500

    master

---
+++ src/app/navigation/bottomnavbar.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector: 'fm-bottomnavbar',
+    templateUrl:'bottomnavbar.component.html',
+    styleUrls: ['bottomnavbar.component.css']
+
+})
+
+
+export class BottomNavBarComponent{
+
+}
---
+++ src/app/navigation/topbarnav.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector: 'fm-topnavbar',
+    templateUrl:'topbarnav.component.html',
+    styleUrls: ['topbarnav.component.css']
+
+})
+
+
+export class TopNavBarComponent{
+
+}
